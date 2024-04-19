b168b17477fb027a8830a4a8f1ebc245357116e0
DrRetro <colindrwho@gmail.com>
2024-04-19 10:38:27 -0400

    Comments and Toggling of Layout Saving

--- Assets/Scripts/TilemapLayoutEditor.cs
+++ Assets/Scripts/TilemapLayoutEditor.cs
@@ -10,8 +10,9 @@ public class TilemapLayoutEditor : MonoBehaviour
 {
     public Tilemap tilemap;
     public string level_name = "";
-    public TextAsset m_level;
-    private TextAsset m_current_level;
+    public bool allow_saving = false;
+    public TextAsset m_level; //Set this with the layout you want to load in when possible.
+    private TextAsset m_current_level; // The level currently loaded into the game.
 	// Start is called before the first frame update
 	void Start()
     {
@@ -24,12 +25,12 @@ public class TilemapLayoutEditor : MonoBehaviour
 
         if (m_current_level != m_level)
         {
-            loadLayout_in_Editor(m_level);
+            Loadlevel(m_level);
         }
     }
     
 
-    void loadLayout_in_Editor(TextAsset layout_to_load) {
+    void Loadlevel(TextAsset layout_to_load) { 
 		LevelData data = JsonUtility.FromJson<LevelData>(layout_to_load.text);
 		tilemap.ClearAllTiles();
 
@@ -40,18 +41,11 @@ public class TilemapLayoutEditor : MonoBehaviour
         m_current_level = m_level;
 	}
 
-    void Loadlevel() { //Loads level from a json file.
-        string json = File.ReadAllText(Application.dataPath + "LevelJSON/test_level.json");
-        LevelData data = JsonUtility.FromJson<LevelData>(json);
-		tilemap.ClearAllTiles();
-
-        for (int i = 0; i < data.poses.Count; i++) {
-            tilemap.SetTile(data.poses[i], data.tiles[i]);
+    void Savelevel() { //ONLY AVAILABLE IN EDITOR.
+		if (!allow_saving) {
+            return;
         }
-	}
-
-    void Savelevel() {
-        BoundsInt bounds = tilemap.cellBounds;
+        BoundsInt bounds = tilemap.cellBounds; //How big is this level?
 
         LevelData levelData = new LevelData();
         
@@ -67,15 +61,15 @@ public class TilemapLayoutEditor : MonoBehaviour
         }
 
         string json = JsonUtility.ToJson(levelData,true);
-        File.WriteAllText(Application.dataPath + "/LevelJSON/"+GetVaildLevelName(), json);
+        File.WriteAllText(Application.dataPath + "/LevelJSON/"+GetVaildLayoutName(), json);
     }
 
-    private string GetVaildLevelName() {
-		var info = new DirectoryInfo(Application.dataPath + "/LevelJSON");
-        var fileInfo = info.GetFiles().ToArray();
-        int depth = 1;
+    private string GetVaildLayoutName() { //For saving. Makes sure that there is no layouts that exist with the same name, and if so, will be given an alternative name.
+		var info = new DirectoryInfo(Application.dataPath + "/LevelJSON"); //Folder full of layout JSONs.
+        var fileInfo = info.GetFiles().ToArray(); //JSON files.
+        int depth = 1; //For making sure the new level has a vaild name, by adding this number at the end.
         while (true) {
-            string temp = level_name+'-'+depth.ToString()+".json";
+            string temp = level_name+'-'+depth.ToString()+".json"; // "test_level-1.json"
             if (File.Exists(Application.dataPath + "/LevelJSON/" + temp)) {
                 depth++;
             }
@@ -89,6 +83,6 @@ public class TilemapLayoutEditor : MonoBehaviour
 
 public class LevelData
 {
-    public List<TileBase> tiles = new List<TileBase>();
-    public List<Vector3Int> poses = new List<Vector3Int>();
+    public List<TileBase> tiles = new List<TileBase>(); //The tiles themselves.
+    public List<Vector3Int> poses = new List<Vector3Int>(); //Positions of those tiles.
 }
